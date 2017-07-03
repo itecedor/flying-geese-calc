28666be9a11b116522d0c68a3d3d492dc3fffa2d
Ivete Tecedor <yivich@gmail.com>
2017-07-03 16:06:12 -0400

    added calculation for cut sizes

--- js/calc.js
+++ js/calc.js
@@ -42,6 +42,44 @@ $(document).ready(function() {
     $(".btn-restart").show();
   });
 
+  // show the cut length calculator
+  $(".btn-cut-length").click(function() {
+    $("#cut-length-form").show();
+    $(".btn-primary").hide();
+  });
+
+  // calculate based on cut length
+  $(".btn-cut-length-calculate").click(function() {
+    var cut_length = parseFloat($("input[name='cut-length']").val());
+    var cut_width = (cut_length - .5)/2 + .5;
+
+    var result_text = 'Size of rectangle to cut: ' + cut_length + ' x ' + cut_width;
+    result_text = result_text + '<p>';
+    result_text = result_text + 'Size of squares to cut: ' + cut_width + ' x ' + cut_width;
+    $("#result").append(result_text);
+
+    $(".btn-restart").show();
+  });
+
+  // show the cut width calculator
+  $(".btn-cut-width").click(function() {
+    $("#cut-width-form").show();
+    $(".btn-primary").hide();
+  });
+
+  // calculate based on cut width
+  $(".btn-cut-width-calculate").click(function() {
+    var cut_width = parseFloat($("input[name='cut-width']").val());
+    var cut_length = (cut_width - .5) * 2 + .5;
+
+    var result_text = 'Size of rectangle to cut: ' + cut_length + ' x ' + cut_width;
+    result_text = result_text + '<p>';
+    result_text = result_text + 'Size of squares to cut: ' + cut_width + ' x ' + cut_width;
+    $("#result").append(result_text);
+
+    $(".btn-restart").show();
+  });
+
   // restart
   $(".btn-restart").click(function() {
     $("input").val("");
